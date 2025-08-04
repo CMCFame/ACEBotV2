3eed76678295ef33aed6726717f549808570925b
CMCFame <jetronico@gmail.com>
2025-08-04 15:23:57 -0600

    ok

--- data/prompts/__init__.py
+++
@@ -1,4 +0,0 @@
-# data/prompts/__init__.py
-"""
-Contains system prompts and instructions for the AI.
-"""
--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# utils/__init__.py
-"""
-ACEBot utilities package.
-Contains helper functions and utilities for the application.
-"""
-
-__all__ = ['helpers']
